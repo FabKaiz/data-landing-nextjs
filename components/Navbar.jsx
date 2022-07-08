fdca88078dde7220225e3741d5b1c8a1668d382b
Fabien Chareun <cacrfabfr@gmail.com>
2022-07-08 10:52:42 +0200

    navbar jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,38 @@
 import styles from '../styles/Navbar.module.scss'
-
+import logo from '../public/assets/headerLogo.png'
+import Image from 'next/image'
+import { GiHamburgerMenu } from 'react-icons/gi'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className={styles.nav}>
+      <div className={styles.navLinks}>
+        <Image src={logo} classname={styles.logo} alt="logo" />
+        <ul>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#gallery">gallery</a>
+          </li>
+          <li>
+            <a href="#pricing">pricing</a>
+          </li>
+          <li>
+            <a href="#faq">faq</a>
+          </li>
+          <li>
+            <a href="#benefits">benefits</a>
+          </li>
+        </ul>
+      </div>
+
+      <div className={styles.navBtns}>
+        <button className="btnTransparent">Sign in</button>
+        <button className="btnFull">Sign up</button>
+      </div>
+      <GiHamburgerMenu className={styles.burgerMenu} />
+    </nav>
   )
 }
 
