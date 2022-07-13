b5cf2a148995bfb7b0366e2b0e961f97e39f57a0
Fabien Chareun <cacrfabfr@gmail.com>
2022-07-13 11:11:08 +0200

    Framework jsx

--- components/Framework.jsx
+++ components/Framework.jsx
@@ -1,9 +1,18 @@
 import styles from '../styles/Framework.module.scss'
-
+import frameworkBg from '../public/assets/frameworkBg.png'
+import Image from 'next/image'
 
 const Framework = () => {
   return (
-    <div>Framework</div>
+    <main>
+      <div className={styles.top}>
+        <h2>Create your <span className="orangeTxt">next project</span> with startup framework</h2>
+        <button className="btnFull">get Started</button>
+      </div>
+      <div className={styles.bottom}>
+        <Image src={frameworkBg} alt="background" />
+      </div>
+    </main>
   )
 }
 
