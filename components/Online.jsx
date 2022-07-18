862941813f1568373a5744ca9a48e7401f5b8ec6
Fabien Chareun <cacrfabfr@gmail.com>
2022-07-18 10:24:46 +0200

    online jsx

--- components/Online.jsx
+++ components/Online.jsx
@@ -1,9 +1,43 @@
 import styles from '../styles/Online.module.scss'
-
+import onlineImg from '../public/assets/onlineImg.png'
+import logo1 from '../public/assets/onlineLogo1.png'
+import logo2 from '../public/assets/onlineLogo2.png'
+import Image from 'next/image'
 
 const Online = () => {
   return (
-    <div>Online</div>
+    <main className={styles.onlineBg}>
+      <div className={styles.left}>
+        <Image src={onlineImg} alt="meeting" />
+      </div>
+      <div className={styles.right}>
+        <div className={styles.top}>
+          <h3>Help Finding Information Online</h3>
+          <p>
+            Fully customizable and neatly organized components will help you
+            work faster without limiting creative freedom.
+          </p>
+        </div>
+        <div className={styles.bottom}>
+          <div>
+            <Image src={logo1} alt="logo" />
+            <h3>Feature One</h3>
+            <p>
+              Fully customizable and neatly organized components will help you
+              work faster
+            </p>
+          </div>
+          <div>
+            <Image src={logo2} alt="logo" />
+            <h3>Feature Two</h3>
+            <p>
+              Fully customizable and neatly organized components will help you
+              work faster
+            </p>
+          </div>
+        </div>
+      </div>
+    </main>
   )
 }
 
