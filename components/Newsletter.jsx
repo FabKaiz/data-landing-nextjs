11b9bff29823e52972f3bc6e5be06969ae43bb9a
Fabien Chareun <cacrfabfr@gmail.com>
2022-07-18 11:01:46 +0200

    format

--- components/Newsletter.jsx
+++ components/Newsletter.jsx
@@ -1,12 +1,14 @@
 import styles from '../styles/Newsletter.module.scss'
 
-
 const Newsletter = () => {
   return (
     <section className={styles.newsletterBg}>
       <div className={styles.newsletterContainer}>
         <h2>There are many reasons to get down</h2>
-        <h5>There are many reasons to get down and start to get depressed about your situation. </h5>
+        <h5>
+          There are many reasons to get down and start to get depressed about
+          your situation.{' '}
+        </h5>
         <div>
           <input type="text" placeholder="Your Email" />
           <button className={styles.newsletterBtn}>send</button>
