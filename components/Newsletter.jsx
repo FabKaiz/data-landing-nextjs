c68303733065f8681889e39e6b7b6a2e9bce0b97
Fabien Chareun <cacrfabfr@gmail.com>
2022-07-18 10:48:13 +0200

    newsletter jsx

--- components/Newsletter.jsx
+++ components/Newsletter.jsx
@@ -3,7 +3,17 @@ import styles from '../styles/Newsletter.module.scss'
 
 const Newsletter = () => {
   return (
-    <div>Newsletter</div>
+    <section className={styles.newsletterBg}>
+      <div className={styles.newsletterContainer}>
+        <h2>There are many reasons to get down</h2>
+        <h5>There are many reasons to get down and start to get depressed about your situation. </h5>
+        <div>
+          <input type="text" placeholder="Your Email" />
+          <button className={styles.newsletterBtn}>send</button>
+        </div>
+        <p>No spam. Only releases, updates and discounts</p>
+      </div>
+    </section>
   )
 }
 
